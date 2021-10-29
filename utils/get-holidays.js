6c72bfebd6a289238363b18432bda7a5149be771
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-10-29 16:49:54 -0500

    fix: attempt to fix incorrect readable date

--- utils/get-holidays.js
+++ utils/get-holidays.js
@@ -1,24 +1,21 @@
 import colombianHolidays from 'colombia-holiday';
 
-const getDateInColombia = (date) => {
-  let actualDate = date ? new Date(date) : new Date();
-  return new Date(actualDate.toLocaleString('en-US', { timeZone: 'America/Bogota' }));
+const formatDate = (date, language = 'es-CO') => {
+  return date.toLocaleString(language, {
+    timeZone: 'America/Bogota',
+    weekday: 'long',
+    year: undefined,
+    month: 'long',
+    day: 'numeric',
+  });
 };
 
 export const getColombianHolidays = (language = 'es-CO') => {
-  const now = getDateInColombia();
+  const now = new Date();
   const mappedHolidays = colombianHolidays().map((holiday) => {
-    const holidayDate = new Date(holiday.holiday);
-    const readableDate = holidayDate.toLocaleString(language, {
-      timeZone: 'America/Bogota',
-      weekday: 'long',
-      year: undefined,
-      month: 'long',
-      day: 'numeric',
-    });
-    holidayDate.setTime(holidayDate.getTime() - 5 * 3600 * 1000);
+    const holidayDate = new Date(`${holiday.holiday.replace(/\//g, '-')}T00:00:00.000-05:00`);
     return {
-      readableDate,
+      readableDate: formatDate(holidayDate, language),
       date: holiday.holiday,
       name: holiday.holidayName,
       diff: now.getTime() - holidayDate.getTime(),
