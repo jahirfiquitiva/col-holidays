e63bf98beb62a314c8c1c41fb9293664cb8cd482
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-10-29 19:03:40 -0500

    feat: support more meta tags. add default title and description

--- src/components/global/meta/meta.tsx
+++ src/components/global/meta/meta.tsx
@@ -1,10 +1,42 @@
 import Head from 'next/head';
 
-import { Component } from '../component';
+import { Component, ComponentProps } from '@/components/global/component';
 
-export const Meta: Component = () => {
+interface MetaProps extends ComponentProps {
+  title?: string;
+  description?: string;
+}
+
+export const Meta: Component<MetaProps> = (props) => {
+  const {
+    title = 'Colombian Holidays | Festivos en Colombia | Feriados en Colombia',
+    // eslint-disable-next-line max-len
+    description = 'Get to know the upcoming colombian holidays. Conoce los próximos festivos o feriados en Colombia',
+  } = props;
   return (
     <Head>
+      <title>{title}</title>
+      <meta name={'title'} content={title} />
+      <meta name={'description'} content={description} />
+
+      <meta property={'og:type'} content={'website'} />
+      <meta property={'og:url'} content={'https://col-holidays.co/'} />
+      <meta property={'og:title'} content={title} />
+      <meta property={'og:description'} content={description} />
+      <meta
+        property={'og:image'}
+        content={'https://col-holidays.co/static/icons/icon-512x512.png'}
+      />
+
+      <meta property={'twitter:card'} content={'summary_large_image'} />
+      <meta property={'twitter:url'} content={'https://col-holidays.co/'} />
+      <meta property={'twitter:title'} content={title} />
+      <meta property={'twitter:description'} content={description} />
+      <meta
+        property={'twitter:image'}
+        content={'https://col-holidays.co/static/banner.png'}
+      />
+
       <link
         rel={'apple-touch-icon'}
         sizes={'57x57'}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
-import Head from 'next/head';
-
 import { Component } from '@/components/global/component';
 import { Layout } from '@/components/global/layout';
 import { Home } from '@/components/home';
 
 const Index: Component = () => {
   return (
-    <>
-      <Head>
-        <title>Colombian Holidays</title>
-        <meta name={'description'} content={'App to show upcoming colombian holidays'} />
-      </Head>
-
-      <Layout>
-        <Home />
-      </Layout>
-    </>
+    <Layout>
+      <Home />
+    </Layout>
   );
 };
 
