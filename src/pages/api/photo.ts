a66d4259a88e2a5c459c7d89468483721ffdc003
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-06-14 18:49:20 -0500

    Use unsplash access token in header

--- src/pages/api/photo.ts
+++ src/pages/api/photo.ts
@@ -3,9 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import { NextApiFunc, PhotoData } from '@/types';
 
-const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
-const searchResults = 10;
-
 interface UnsplashPhoto {
   width: number;
   height: number;
@@ -25,6 +22,24 @@ interface UnsplashPhoto {
   };
 }
 
+// const defaultPhoto: PhotoData = {
+//   width: photo.width,
+//   height: photo.height,
+//   color: photo.color,
+//   blur_hash: photo.blur_hash,
+//   url: photo.urls.regular,
+//   description: photo.location?.title || '',
+//   alt_description: photo.alt_description,
+//   link: photo.links.html || '',
+// };
+
+const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
+const searchResults = 10;
+
+const requestHeaders = new Headers();
+requestHeaders.append('Content-Type', 'application/json');
+requestHeaders.append('Authorization', `Client-ID ${unsplashAccessKey}`);
+
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse,
@@ -32,15 +47,24 @@ const handler = async (
   const { country } = req.query;
   const actualCountry = Array.isArray(country) ? country[0] : country;
   const request = await fetch(
-    `https://api.unsplash.com/photos/random?client_id=${unsplashAccessKey}&query=${actualCountry}%20landscape%20nature&orientation=landscape&count=${searchResults}&content_filter=high`,
+    `https://api.unsplash.com/photos/random?query=${actualCountry}%20landscape%20nature&orientation=landscape&count=${searchResults}&content_filter=high`,
+    {
+      method: 'GET',
+      headers: requestHeaders,
+    },
   );
-  const response = await request.json();
+
   const { status } = request;
-  if (!response) {
-    return res.status(status).json({});
+  if (status < 200 || status >= 400) {
+    const responseText = await request.text();
+    return res.status(status).json({
+      // @ts-ignore
+      error: responseText || 'Unsplash API error. Probably rate limit',
+    });
   }
 
   try {
+    const response = await request.json();
     const photo: UnsplashPhoto = (response || []).filter(
       (it: UnsplashPhoto) => {
         return (
